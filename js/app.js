434de4fab6784170b07595e3b1b52f514b2ca16e
Student_MA122 <student_ma122@Student-MA122.local>
2020-03-13 10:06:55 -0400

    added comp logic in js

--- js/app.js
+++ js/app.js
@@ -33,90 +33,117 @@ let yourAttack = Math.floor((Math.random()*3)+1);
 
   let knights = [enemyKnight1,enemyKnight2,enemyKnight3,enemyKnight4,enemyKnight5];
   
-  let n = 0;
-  // knights[n].headHealth display this in html
+let n = 0;
+// knights[n].headHealth display this in html
 //users knight dashboard
-  document.getElementById("scoreNumHead").innerHTML = yourKnight.headHealth;
-  document.getElementById("scoreNumBody").innerHTML = yourKnight.bodyHealth;
-  document.getElementById("scoreNumLegs").innerHTML = yourKnight.legsHealth;
+document.getElementById("scoreNumHead").innerHTML = yourKnight.headHealth;
+document.getElementById("scoreNumBody").innerHTML = yourKnight.bodyHealth;
+document.getElementById("scoreNumLegs").innerHTML = yourKnight.legsHealth;
 //computer knight dashboard
-  document.getElementById("scoreCompHead").innerHTML = knights[n].headHealth;
-  document.getElementById("scoreCompBody").innerHTML = knights[n].bodyHealth;
-  document.getElementById("scoreCompLegs").innerHTML = knights[n].legsHealth;
+document.getElementById("scoreCompHead").innerHTML = knights[n].headHealth;
+document.getElementById("scoreCompBody").innerHTML = knights[n].bodyHealth;
+document.getElementById("scoreCompLegs").innerHTML = knights[n].legsHealth;
 
 
-  
 
-  let usersChoice = 0;
+let dayTime = 0;
 
-  let dayTime = 0;
+let usersChoice = null;
+function user_btn1 (){
+    usersChoice = 1;
+};
+function user_btn2 (){
+    usersChoice = 2;
+};
+function user_btn3 (){
+    usersChoice = 3;
+};
 
+let hitHead_comp = knights[n].headHealth;
+let hitBody_comp = knights[n].bodyHealth;
+let hitLegs_comp = knights[n].legsHealth;
 
-// that's for comp attack, should be btw 1-3, to choose what part of your body to hit
-
-  let compRandom = Math.floor(Math.random(3)+1);
-  //
-  console.log('compRandom = ' + compRandom);
-  //
-  function attack () {
-      usersChoice = 1;
-      console.log('invokes a function')
+function attack () {
+    console.log('invokes a function')
       //check you're not dead
     if (yourKnight.bodyHealth > 0 & yourKnight.headHealth > 0 & yourKnight.legsHealth > 0) {
         console.log('checked user')
         // check comp is alive
-        if (knights[n].headHealth > 0 & knights[n].bodyHealth > 0 & knights[n].legsHealth > 0 ) {
+        if (hitHead_comp > 0 & hitBody_comp > 0 & hitLegs_comp > 0 ) {
             console.log('checked comp')
             // check your accuracy
             if (Math.random() < yourKnight.accuracy) {  
                 // check user's choice
-                switch(1) {
+                switch(usersChoice) {
                     case 1:
-                        let bodyAttack = knights[n].bodyHealth - yourKnight.attack;
-                        document.getElementById("scoreCompHead").innerHTML = bodyAttack;
-                        console.log('compRandomCase1');
+                        hitHead_comp -= yourKnight.attack;
+                        document.getElementById("scoreCompHead").innerHTML = hitHead_comp;
+                        console.log('hit Head of comp ' + hitHead_comp);
+                        compAttack();
                         break;
                     case 2:
-                        knights[n].headHealth - yourKnight.attack;
+                        hitBody_comp -= yourKnight.attack;
+                        document.getElementById("scoreCompBody").innerHTML = hitBody_comp;
+                        console.log('hit Body of comp ' + hitHead_comp);
+                        compAttack();
                         break;
                     case 3:
-                        knights[n].legsHealth - yourKnight.attack;
+                        hitBody_comp -= yourKnight.attack;
+                        document.getElementById("scoreCompLegs").innerHTML = hitBody_comp;
+                        console.log('hit Legs of comp ' + hitHead_comp);
+                        compAttack();
                         break;
                     default:
                         break;    
                 }
             }
-            //switching to enemies attack
-            
-            if (Math.random() < knights[n].accuracy) {
-            
-                switch(compRandom) {
-                    case 1:
-                        let hitHead = yourKnight.bodyHealth -  knights[n].attack;
-                        document.getElementById("scoreNumHead").innerHTML = hitHead;
-                        console.log('compRandomCase1');
-                        break;
-                    case 2:
-                        let hitBody = yourKnight.headHealth -  knights[n].attack;
-                        document.getElementById("scoreCompBody").innerHTML = hitBody;
-                        console.log('compRandomCase2');
-                        break;
-                    case 3:
-                        let hitLegs = yourKnight.legsHealth -  knights[n].attack;
-                        document.getElementById("scoreCompLegs").innerHTML = hitLegs;
-                        console.log('compRandomCase3');
-                        break;
-                    default:
-                        break;    
-                }
-        
-            }        
+            else {
+                console.log('You missed attack')
+            }
         }
         else {
-            // comp dead
             n++;
-        }    
-    }
-  }
+            document.getElementById("yourKillsNum").innerHTML = n;
+            console.log(' ** Comp died ' + n);
+            
+        }  
+    }          
+    else {
+       console.log('You died like a hero '); 
+       // add style of your death display none and then appear
+    }    
+}
 
-  // create random accuracy for computer
+
+let hitHead_comp1 = yourKnight.headHealth;
+let hitBody_comp2 = yourKnight.bodyHealth;
+let hitLegs_comp3 = yourKnight.legsHealth;
+
+
+function compAttack () {
+    if (Math.random() < knights[n].accuracy) {
+        let compRandom = Math.floor((Math.random()*3)+1);
+        switch(compRandom) {
+            case 1:
+                hitHead_comp1 -= knights[n].attack;
+                document.getElementById("scoreNumHead").innerHTML = hitHead_comp1;
+                console.log("user CompHead" + hitHead_comp);
+                break;
+            case 2:
+                hitBody_comp2 -= knights[n].attack;
+                document.getElementById("scoreNumBody").innerHTML = hitBody_comp2;
+                console.log("user CompBody" + hitBody_comp2);
+                break;
+            case 3:
+                hitLegs_comp3 -= knights[n].attack;
+                document.getElementById("scoreNumLegs").innerHTML = hitLegs_comp3;
+                console.log("user CompLegs" + hitLegs_comp3);
+                break;
+            default:
+                break;    
+        }
+    }
+    else {
+        console.log('computer missed');
+    }
+}
