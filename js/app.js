ca066da7a5e5e666194f713c994f40d9ff59cf8c
Gypsy4 <57001092+Gypsy4@users.noreply.github.com>
2020-03-16 20:54:29 -0400

    add black background if u died

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@ function randomNum(min, max) {
   let enemyKnight3 = new knight();
   let enemyKnight4 = new knight();
   let enemyKnight5 = new knight();
+  let enemyKnight6 = new knight();
+  let enemyKnight7 = new knight();
+  let enemyKnight8 = new knight();
+  let enemyKnight9 = new knight();
+  let enemyKnight10 = new knight();
 
 let yourAttack = Math.floor((Math.random()*3)+1);
 
@@ -29,7 +34,7 @@ let yourAttack = Math.floor((Math.random()*3)+1);
     legsHealth:21
   }
 
-let knights = [enemyKnight1,enemyKnight2,enemyKnight3,enemyKnight4,enemyKnight5];
+let knights = [enemyKnight1,enemyKnight2,enemyKnight3,enemyKnight4,enemyKnight5,enemyKnight6,enemyKnight7,enemyKnight8,enemyKnight9,enemyKnight10];
   
 let n = 0;
 // knights[n].headHealth display this in html
@@ -59,9 +64,9 @@ function user_btn3 (){
     usersChoice = 3;
 };
 
-let hitHead_comp = knights[n].headHealth;
-let hitBody_comp = knights[n].bodyHealth;
-let hitLegs_comp = knights[n].legsHealth;
+// let hitHead_comp = knights[n].headHealth;
+// let hitBody_comp = knights[n].bodyHealth;
+// let hitLegs_comp = knights[n].legsHealth;
 
 let dayTime = 0;
 
@@ -69,41 +74,32 @@ let userBlock;
 let compBlock;
 
 function attack () {
-    // delete or finish
-    // dayTime++;
-    // if (dayTime%3 === 0 & dayTime%2 === 0) {
-    //     img.src = "";
-    // }
+    
     console.log('invokes a function')
     //check you're not dead
     if (hitHead_comp1 > 0 & hitBody_comp2 > 0 & hitLegs_comp3 > 0) {
         console.log('checked user')
         // check comp is alive
-        if (hitHead_comp > 0 & hitBody_comp > 0 & hitLegs_comp > 0 ) {
+        if (knights[n].headHealth > 0 & knights[n].bodyHealth > 0 & knights[n].legsHealth > 0 ) {
             console.log('checked comp')
-            // check your accuracy
-            userBlock = prompt('What part of body you want to block: 1)H for HEAD  2) B for BODY 3) L for LEGS' , 'H');
-            compBlock = Math.floor(((Math.random)*3)+1);
-            if (userBlock)
-            // compare with computer attack choice
             if (Math.random() < yourKnight.accuracy) {  
                 // check user's choice
                 switch(usersChoice) {
                     case 1:
-                        hitHead_comp -= yourKnight.attack;
-                        document.getElementById("scoreCompHead").innerHTML = hitHead_comp;
+                        knights[n].headHealth -= yourKnight.attack;
+                        document.getElementById("scoreCompHead").innerHTML = knights[n].headHealth;
                         document.getElementById("user_info").innerHTML = "We att HEAD by " + yourKnight.attack;
                         compAttack();
                         break;
                     case 2:
-                        hitBody_comp -= yourKnight.attack;
-                        document.getElementById("scoreCompBody").innerHTML = hitBody_comp;
+                        knights[n].bodyHealth -= yourKnight.attack;
+                        document.getElementById("scoreCompBody").innerHTML = knights[n].bodyHealth;
                         document.getElementById("user_info").innerHTML = "We att BODY by " + yourKnight.attack;
                         compAttack();
                         break;
                     case 3:
-                        hitBody_comp -= yourKnight.attack;
-                        document.getElementById("scoreCompLegs").innerHTML = hitBody_comp;
+                        knights[n].legsHealth -= yourKnight.attack;
+                        document.getElementById("scoreCompLegs").innerHTML = knights[n].legsHealth;
                         document.getElementById("user_info").innerHTML = "We att LEGS by " + yourKnight.attack;
                         compAttack();
                         break;
@@ -119,13 +115,15 @@ function attack () {
         else {
             document.getElementById("user_info").innerHTML = "You killed a knight";
             document.getElementById("comp_info").innerHTML = " R.I.P."
-            document.getElementById("rightKnight").style.opacity = "0.00003";
+
             n++;
             document.getElementById("yourKillsNum").innerHTML = n;
         }  
     }          
     else {
         document.getElementById("user_info").innerHTML = "You died like a honorable knight";
+        document.getElementById("wrapper").style.background = "black";
+        document.getElementById("header").style.opacity = "0.4";
     }    
 }
 
